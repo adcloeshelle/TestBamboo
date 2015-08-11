e597125e384e7be04ef3b93f72b5202fbe24bb27
Joel Loeshelle <adcuser@ADCs-MacBook-Pro.local>
2015-08-11 14:57:39 -0400

    First commit

---
+++ TestBranching/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestBranching
+//
+//  Created by adcuser on 8/11/15.
+//  Copyright (c) 2015 Alarm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestBranching/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestBranching
+//
+//  Created by adcuser on 8/11/15.
+//  Copyright (c) 2015 Alarm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
